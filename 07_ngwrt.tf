a17e0789974e09f18b399b999035c54011fd4702
langsang0221 <langsang0221@gmail.com>
2021-11-25 17:10:04 +0900

    'step1'

---
+++ 07_ngwrt.tf
@@ -0,0 +1,11 @@
+resource "aws_route_table" "ityoon_ngwrt" {
+  vpc_id = aws_vpc.ityoon_vpc.id
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.ityoon_ngw.id
+
+  }
+  tags = {
+    "Name" = "ityoon_ngwrt"
+  }
+}
