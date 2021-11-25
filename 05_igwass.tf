c000ee0dffc7314def296d831b3c9faa85c317f2
langsang0221 <langsang0221@gmail.com>
2021-11-25 16:39:53 +0900

    'step2'

--- 05_igwass.tf
+++ 05_igwass.tf
@@ -1,10 +1,10 @@
 # public a
 resource "aws_route_table_association" "ityoon_igw_puba" {
-  subnet_id = aws_subnet.ityoon_puba.id
+  subnet_id      = aws_subnet.ityoon_puba.id
   route_table_id = aws_route_table.ityoon_rt.id
 }
 # public c
 resource "aws_route_table_association" "ityoon_igwa_pubc" {
-  subnet_id = aws_subnet.ityoon_pubc.id
+  subnet_id      = aws_subnet.ityoon_pubc.id
   route_table_id = aws_route_table.ityoon_rt.id
 }
