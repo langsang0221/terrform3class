33f0e4e9207615f0804b84677c7cf9c8afa80497
langsang0221 <langsang0221@gmail.com>
2021-11-26 16:55:55 +0900

    'step1'

---
+++ 17_atsg.tf
@@ -0,0 +1,17 @@
+resource "aws_placement_group" "ityoon_pg" {
+  name     = "ityoon-pg"
+  strategy = "cluster"
+}
+
+resource "aws_autoscaling_group" "ityoon_atsg" {
+  name                      = "ityoon-atsg"
+  min_size                  = 2
+  max_size                  = 10
+  health_check_grace_period = 60
+  health_check_type         = "EC2"
+  desired_capacity          = 2
+  force_delete              = true
+  launch_configuration      = aws_launch_configuration.ityoon_lacf.name
+  #placement_group           = aws_placement_group.ityoon_pg.id
+  vpc_zone_identifier       = [aws_subnet.ityoon_puba.id, aws_subnet.ityoon_pubc.id]
+}
