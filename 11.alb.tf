7a1c6058ddbc91566b68f92420a75ac68af108ae
langsang0221 <langsang0221@gmail.com>
2021-11-26 12:12:06 +0900

    'step1'

---
+++ 11.alb.tf
@@ -0,0 +1,10 @@
+resource "aws_lb" "ityoon_alb" {
+  name               = "ityoon-alb"
+  internal           = false
+  load_balancer_type = "application"
+  security_groups    = [aws_security_group.ityoon_sg.id]
+  subnets            = [aws_subnet.ityoon_puba.id, aws_subnet.ityoon_pubc.id]
+  tags = {
+    Name = "ityoon_alb"
+  }
+}
